ec6001c20ab9f8274474e1569fcabac6d4e9d915
Arnavv28 <chauhanarnav284@gmail.com>
2026-09-03 14:52:07 +0530

    03/09

--- LAB3/prg11.js
+++ LAB3/prg11.js
@@ -1,62 +1,67 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    if(req.url ==='/' && req.method === 'GET')
+
+    if (req.url === '/' && req.method === 'GET') {
         res.end('home page');
-    else if(req.url === '/product' && req.method ==='GET'){
-        const products = [
-        {
-            id: 1,
-            name: "mobile",
-            price : 20000
-        },
-        {
-            id : 2,
-            name : "duster",
-            price : 100
-        },
-        ];
-            
-        res.end(JSON.stringify(products));
-}
-    else if(req.url === '/product' && req.method ==='POST'){
-            // retrieve data from client
-            let body = " " ;
-            req.on("data",(chunk)=>{
-                body += chunk
-            });
-            req.on("end",() => {
-                const product = JSON.parse(body);
-
-            //add data to database
-                res.writeHead(201,{
-                "content-type":"application/json"
-            });
-            //send back to status
-            res.end(
-                JSON.stringify({
-
-            }
-            }
-
-            
-            
-            
-            
-
-        
     }
 
-    else if(req.url === '/product' && req.method ==='PUT')
-        res.end('update product')
+    else if (req.url === '/product' && req.method === 'GET') {
 
-    else if(req.url === '/product' && req.method=== 'DELETE')
-        res.end('remove product')
+        const product = [
+            {
+                id: 1,
+                name: "mobile",
+                price: 2000,
+            },
+            {
+                id: 2,
+                name: "duster",
+                price: 10,
+            }
+        ];
 
-   else{
-    res.statusCode = 404;
-    res.end("not found");
-   } 
-})
+        res.end(JSON.stringify(product));
+    }
 
-server.listen(3300,()=> console.log("prg11 is running on port 3300..."));
+    else if (req.url === '/product' && req.method === 'POST') {
+
+        // retrieve data from client
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        req.on("end", () => {
+
+            const product = JSON.parse(body);
+
+            // add data to database
+            res.writeHead(201, {
+                "content-type": "application/json"
+            });
+
+            // send back the status
+            res.end(JSON.stringify({
+                msg: "product added",
+                product: product
+            }));
+        });
+    }
+
+    else if (req.url === '/product' && req.method === 'PUT') {
+        res.end('update product');
+    }
+
+    else if (req.url === '/product' && req.method === 'DELETE') {
+        res.end('remove product');
+    }
+
+    else {
+        res.statusCode = 404;
+        res.end("not found");
+    }
+});
+
+server.listen(3000, () => console.log("prg11 is running..."));
