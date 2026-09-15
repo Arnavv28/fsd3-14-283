8e9f9a7bd910c2f35a20dd740d98c95d8f54f7f3
Arnavv28 <chauhanarnav284@gmail.com>
2026-09-15 09:41:44 +0530

    15/09

--- lab4/teams.js
+++ lab4/teams.js
@@ -1,42 +1,51 @@
 let teams = [
-    { 
-        id :1,
-        tname:"Rusty",
-        tl:"Ashish Raj Singh",
-        email:"ashishraj@gmail.com",
-        members:6,
+    {
+        id: 1,
+        tname: "Rusty",
+        tl: "Arpit Agnihotri",
+        email: "arpit151604@gmail.com",
+        members: 6
     },
     {
-        id:2,
-        tname:"ARGUS",
-        tl:"Archit Shrivastava",
-        email: "archit@gmail.com",
-        members:6,
-    },
+        id: 2,
+        tname: "Acube",
+        tl: "Archit Shrivastava",
+        email: "archit314@gmail.com",
+        members: 3
+    }
 ];
 
 let nextId = 3;
 
 export const getAllTeams = () => teams;
 
-export const getTeamById = (id) => teams.find((team) => team.id === id);
+export const getTeamById = (id) =>
+    teams.find((team) => team.id === id);
 
-export const addTeam = (newTeam) =>{
-    const team =  {id:nextId++, ...newTeam};
+export const addTeam = (newTeam) => {
+    const team = {
+        id: nextId++,
+        ...newTeam
+    };
+
     teams.push(team);
     return team;
 };
 
-export const updateTeamById = (id, updateTeam) =>{
-    const team  = getTeamById(id);
+export const updateTeam = (id, updatedTeam) => {
+    const team = getTeamById(id);
+
     if (!team) return null;
-    Object.assign(team,updateTeam);
-    return team; 
+
+    Object.assign(team, updatedTeam);
+    return team;
 };
 
 export const deleteTeam = (id) => {
     const index = teams.findIndex((team) => team.id === id);
-    if (index == -1) return false;
-    teams.splice(index ,1);
+
+    if (index === -1) return false;
+
+    teams.splice(index, 1);
     return true;
 };
