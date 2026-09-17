74a18adb18d84d8eef000628d02dac9bc3bd72aa
Arnavv28 <chauhanarnav284@gmail.com>
2026-09-17 13:58:50 +0530

    basic routing and express server

--- lab5/app.js
+++ lab5/app.js
@@ -2,12 +2,30 @@ import express from 'express'
 
 const app = express();
 
-app.get("/",(req,res) => {
-    res.send("<h1>Hello Express</h1>");
+app.get("/", (req, res) => {
+    res.send("<h1> Hello Express </h1>");
 });
 
-/*app.use("*",(req,res) => {
-    res.send("Route not found");
-});*/
+app.get('/about', (req, res) => {
+    res.end("We are Full Stack Developer")
+});
 
-app.listen(3000,()=> console.log("Server IS Running"));
+app.post('/login', (req, res) => {
+    res.send({msg: 'user login'})
+});
+
+app.put('/user/update/1', (req, res) => {
+    res.send({msg: 'user update'})
+});
+
+app.delete('/users/1', (req, res) => {
+    res.send({msg: 'remove user 1'})
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found")
+});
+
+
+
+app.listen(3333, () => console.log("Server is running "));
